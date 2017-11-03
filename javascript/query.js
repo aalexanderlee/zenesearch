e8a8d0a38e8c3c327e08507bd17199d6861db35e
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-11-03 03:41:34 -0700

    Debugged and confirmed Firebase is working. Next step is to restructure Google Places API callback and remove AJAX method.

--- javascript/query.js
+++ javascript/query.js
@@ -1,21 +1,20 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCS4KuIbLP5fBej1eOxbNdS3ssOQijwadg",
-    authDomain: "test1-49f56.firebaseapp.com",
-    databaseURL: "https://test1-49f56.firebaseio.com",
-    projectId: "test1-49f56",
-    storageBucket: "test1-49f56.appspot.com",
-    messagingSenderId: "373692440483"
-};
-
+    apiKey: "AIzaSyCpkdbr1OLEgZ7xYuZX4J9S0jiFaWLGKgk",
+    authDomain: "zenesearch.firebaseapp.com",
+    databaseURL: "https://zenesearch.firebaseio.com",
+    projectId: "zenesearch",
+    storageBucket: "zenesearch.appspot.com",
+    messagingSenderId: "391059269856"
+  };
 firebase.initializeApp(config);
 var resultCounter = 0;
 var map;
 var markers = [];
 var database = firebase.database();
 
-var tl = new TimelineMax({repeat:600000, repeatDelay:1, yoyo:true});
-tl.staggerTo("h1,h2", 0.2, {className:"+=superShadow", top:"-=10px", ease:Power1.easeIn}, "0.3", "start")
+// var tl = new TimelineMax({repeat:600000, repeatDelay:1, yoyo:true});
+// tl.staggerTo("h1,h2", 0.2, {className:"+=superShadow", top:"-=10px", ease:Power1.easeIn}, "0.3", "start")
 
 
 function validateForm() {
@@ -48,21 +47,17 @@ function initMap() {
 
   clearMapDiv();
 
-  var options = {
-    zoom: 10,
-    center: {lat: 37.773972, lng: -122.431297}
-  };
-
-  var map = new google.maps.Map(document.getElementById('map'), options);
+  var map = new google.maps.Map(document.getElementById('map'), {zoom: 10, center:{lat:37.7749, lng:-122.4194}});
 
   database.ref().limitToLast(10).on("child_added", function(snapshot) {
     var sv = snapshot.val();
 
-        var tribeca = {lat: sv.latitude, lng: sv.longitude};
+        var center = {lat: sv.latitude, lng: sv.longitude};
         var marker = new google.maps.Marker({
-          position: tribeca,
+          position: center,
           map: map,
         });
+        map.panTo(center);
 
         var companies = sv.company;
 
@@ -89,9 +84,10 @@ function getData() {
   var radius = $('input[type="radio"]:checked').val();
 
   // Insert before queryURL if jsonp doesn't work - "https://cors.now.sh/"
-  // var queryURL = /*"https://cors.now.sh/"+*/"https://api.indeed.com/ads/apisearch?publisher=1665103808901378&q="+title+"&l="+city+"%2C+"+state+"&sort=&radius="+radius+"&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json"
+  // var queryURL = /*"https://cors.now.sh/"+*/"https://api.indeed.com/ads/apisearch?publisher=1665103808901378&q="+topic+"&l="+city+"%2C+"+state+"&sort=&radius="+radius+"&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json"
   // Original coordinates from Places API = &location=42.3675294,-71.186966
-  var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+topic+"+"+city+"+"+state+"&radius="+radius+"&key=AIzaSyD4W7R9ba_TgScFBewx6wp5VE5zpffpuAY"
+  // Original key = AIzaSyD4W7R9ba_TgScFBewx6wp5VE5zpffpuAY
+  var queryURL = "https://cors.now.sh/"+"https://maps.googleapis.com/maps/api/place/textsearch/json?query="+topic+"+"+city+"+"+state+"&radius="+radius+"&key=AIzaSyAEqiSu63n6-F2BKTTuF_CnvsTpyUsYiNM";
 
 
   //Call on Indeed queryURL
@@ -114,16 +110,26 @@ function getData() {
       // var jobLink = response.results[i].jobtitle
 
       //Appends new divs
+      // $('#table > tbody')
+      //   .append('<tr>'+tableHead+resultCounter+'</th><td><a href="'+response.results[i].url+'" target="_blank"><h2>'+response.results[i].jobtitle+'</h2></a></td><td><h4>'+
+      //   response.results[i].company+'</h4></td><td><p>'+response.results[i].snippet+'</p></td></tr>');
+
       $('#table > tbody')
         .append('<tr>'+tableHead+resultCounter+'</th><td><a href="'+response.results[i].photos[i].html_attributions[0]+'" target="_blank"><h2>'+response.results[i].name+'</h2></a></td><td><h4>'+
         response.results[i].rating+'</h4></td><td><p>'+response.results[i].formatted_address+'</p></td></tr>');
 
-
       //Define search result terms
       var newSearch = {
-        // topic: response.results[i].type,
-        topic: response.results[i].name,
-        location: response.results[i].formatted_address,
+      //  job_title: response.results[i].jobtitle,
+      //  location: response.results[i].formattedLocation,
+      //  company: response.results[i].company,
+      //  latitude: response.results[i].latitude,
+      //  longitude: response.results[i].longitude,
+      //  dateAdded: firebase.database.ServerValue.TIMESTAMP,
+        html_attributions: response.results[i].photos[i].html_attributions[0],
+        name: response.results[i].name,
+        rating: response.results[i].rating,
+        formatted_address: response.results[i].formatted_address,
         latitude: response.results[i].geometry.location.lat,
         longitude: response.results[i].geometry.location.lng,
         dateAdded: firebase.database.ServerValue.TIMESTAMP,
