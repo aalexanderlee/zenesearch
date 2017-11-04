a6e136f4ef3987d396b5174f3c56a5e544d660a5
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-11-04 12:40:43 -0700

    Adding Places API Docs branch altering getData() to retrieve callback request

--- javascript/query.js
+++ javascript/query.js
@@ -1,4 +1,4 @@
-// Initialize Firebase
+// Firebase is initialized to pass and retrieve request data from Google Places API.
 var config = {
     apiKey: "AIzaSyCpkdbr1OLEgZ7xYuZX4J9S0jiFaWLGKgk",
     authDomain: "zenesearch.firebaseapp.com",
@@ -8,124 +8,118 @@ var config = {
     messagingSenderId: "391059269856"
   };
 firebase.initializeApp(config);
+
+// Initialize result counter as the returned data entry IDs
 var resultCounter = 0;
 var map;
 var markers = [];
 var database = firebase.database();
 
-// var tl = new TimelineMax({repeat:600000, repeatDelay:1, yoyo:true});
-// tl.staggerTo("h1,h2", 0.2, {className:"+=superShadow", top:"-=10px", ease:Power1.easeIn}, "0.3", "start")
+// The Green Sock library supplies the header's motion effect.
+var tl = new TimelineMax({repeat:600000, repeatDelay:1, yoyo:true});
+tl.staggerTo("h1,h2", 0.2, {className:"+=superShadow", top:"-=10px", ease:Power1.easeIn}, "0.3", "start")
 
-
+// This function will distinguish input errors and notify.
 function validateForm() {
-    //Clear the search-results div
+    // Clear the search-results div.
     $('#table > tbody').empty();
-
+    // Set values from input boxes to variables.
     var x = $('#topic-input').val().trim();
     console.log("x", x);
     var y = $('#city-input').val().trim();
     console.log("y", y);
     var z = $('#state-input').val().trim();
     console.log("z", z);
-
+    // Check to confirm they were filled out.
     if (x == "" || y == "" || z == "")  {
       $("#search-results").append('<div id="error"> Please fill out the appropriate sections. </div>');
     }
     else {
       $("#error").empty();
+      initMap();
       getData();
     }
 }
 
-
-
+// Clear the map of previous map.
 function clearMapDiv(){
   $('#map').empty();
 }
 
+// Initialize the map after the clearing previous version.
 function initMap() {
-
   clearMapDiv();
-
+  var yourTopic = $("#topic-input").val().trim();
+  topic = formatQueryString(yourTopic);
+  // Center the new map in San Francisco proper.
   var map = new google.maps.Map(document.getElementById('map'), {zoom: 10, center:{lat:37.7749, lng:-122.4194}});
-
+  // Grab the last saved data from Firebase.
   database.ref().limitToLast(10).on("child_added", function(snapshot) {
     var sv = snapshot.val();
-
         var center = {lat: sv.latitude, lng: sv.longitude};
         var marker = new google.maps.Marker({
           position: center,
           map: map,
         });
         map.panTo(center);
-
         var companies = sv.company;
-
         var infowindow = new google.maps.InfoWindow({
           content: companies
         })
         marker.addListener('click', function() {
           infowindow.open(map, marker);
         });
-    });
+        //Here you add the fields you require for request for PlacesService()
+        var request = {
+          location: center,
+          radius: $('input[type="radio"]:checked').val(),
+          types: ['topic']
+        };
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);
+  });
 }
 
 
 function getData() {
+  initMap();
+  // These are our user inputs and search parameters
+  //var Arr = [];
   var yourTopic = $("#topic-input").val().trim();
   topic = formatQueryString(yourTopic);
-
+  //Arr.push(topic);
   var location = $("#city-input").val().trim();
   city = formatQueryString(location);
-
+  //Arr.push(city);
   var state = $("#state-input").val().trim();
   state = formatQueryString(state);
-
+  //Arr.push(state);
+  var searchTerm = topic+city+state;
   var radius = $('input[type="radio"]:checked').val();
 
-  // Insert before queryURL if jsonp doesn't work - "https://cors.now.sh/"
-  // var queryURL = /*"https://cors.now.sh/"+*/"https://api.indeed.com/ads/apisearch?publisher=1665103808901378&q="+topic+"&l="+city+"%2C+"+state+"&sort=&radius="+radius+"&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json"
-  // Original coordinates from Places API = &location=42.3675294,-71.186966
-  // Original key = AIzaSyD4W7R9ba_TgScFBewx6wp5VE5zpffpuAY
-  var queryURL = "https://cors.now.sh/"+"https://maps.googleapis.com/maps/api/place/textsearch/json?query="+topic+"+"+city+"+"+state+"&radius="+radius+"&key=AIzaSyAEqiSu63n6-F2BKTTuF_CnvsTpyUsYiNM";
+  function produceSearch(searchTerm) {
+    var _params = {
+        radius : radius,
+        type : ['searchTerm'],
+        location : app.modules.mapsProvider.getLatLng(position),
+        key : 'AIzaSyAEqiSu63n6-F2BKTTuF_CnvsTpyUsYiNM'
+    };
+    var service = new google.maps.places.PlacesService(type); //map_inst
+    service.search(_params, requestSucceeded);
+    console.log ("Request succeeded: " + requestSucceeded);
 
-
-  //Call on Indeed queryURL
-  $.ajax({
-    url: queryURL,
-    dataType: "jsonp",
-    method: "GET"
-    }).done(function(response) {
-
-    answer = response;
-    console.log("Query URL: "+queryURL);
-
-    //Iterate through Indeed response
     for (var i=0; i<response.results.length; i++) {
       resultCounter++;
 
       var tableHead = $("<th>");
       tableHead.attr("scope", "row");
       tableHead.attr("id", "result-"+resultCounter);
-      // var jobLink = response.results[i].jobtitle
-
-      //Appends new divs
-      // $('#table > tbody')
-      //   .append('<tr>'+tableHead+resultCounter+'</th><td><a href="'+response.results[i].url+'" target="_blank"><h2>'+response.results[i].jobtitle+'</h2></a></td><td><h4>'+
-      //   response.results[i].company+'</h4></td><td><p>'+response.results[i].snippet+'</p></td></tr>');
 
       $('#table > tbody')
         .append('<tr>'+tableHead+resultCounter+'</th><td><a href="'+response.results[i].photos[i].html_attributions[0]+'" target="_blank"><h2>'+response.results[i].name+'</h2></a></td><td><h4>'+
         response.results[i].rating+'</h4></td><td><p>'+response.results[i].formatted_address+'</p></td></tr>');
 
-      //Define search result terms
       var newSearch = {
-      //  job_title: response.results[i].jobtitle,
-      //  location: response.results[i].formattedLocation,
-      //  company: response.results[i].company,
-      //  latitude: response.results[i].latitude,
-      //  longitude: response.results[i].longitude,
-      //  dateAdded: firebase.database.ServerValue.TIMESTAMP,
         html_attributions: response.results[i].photos[i].html_attributions[0],
         name: response.results[i].name,
         rating: response.results[i].rating,
@@ -134,21 +128,17 @@ function getData() {
         longitude: response.results[i].geometry.location.lng,
         dateAdded: firebase.database.ServerValue.TIMESTAMP,
       };
-
       //Push search results to Firebase
       database.ref().push(newSearch);
-
-    } //Close for loop
-
-  }); //Close ajax response
-
-} //Close getData function
+    }
+  }
+} // Close getData()
 
 
 function formatQueryString(str) {
     var finalString;
     var splitString = str.split(" ");
-
+    // If there was a space, concatenate it.
     if (splitString.length > 1) {
       finalString = splitString.join("+");
     }
@@ -160,18 +150,10 @@ function formatQueryString(str) {
 
 //Calls document functions
 $(document).ready(function(){
-
-
-$(".panel-body").on("click", "#submit-button", function(event) {
-  event.preventDefault();
-
-  //Automatically scroll down to search-results div
-  $('html,body').animate({
-        scrollTop: $("#search-results").offset().top},
-        'slow');
-
-  validateForm();
-
-});//onclick
-
-}); // document on ready
+  $(".panel-body").on("click", "#submit-button", {passive: true}, function(event) {
+    event.preventDefault();
+    //Automatically scroll down to search-results div
+    $('html,body').animate({scrollTop: $("#search-results").offset().top}, 'slow');
+    validateForm();
+  });// On click trigger.
+}); // Document on ready
